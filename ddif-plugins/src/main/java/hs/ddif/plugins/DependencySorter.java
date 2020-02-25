af62d3cb0c9964b7de32acf177debe3960593c50
John Hendrikx <hjohn@xs4all.nl>
2020-02-25 09:09:56 +0100

    Add Producer support for Plugins so registration of resulting classes can proceed in the correct order

--- ddif-plugins/src/main/java/hs/ddif/plugins/DependencySorter.java
+++ ddif-plugins/src/main/java/hs/ddif/plugins/DependencySorter.java
@@ -1,5 +1,6 @@
 package hs.ddif.plugins;
 
+import hs.ddif.core.Producer;
 import hs.ddif.core.ProvidedInjectable;
 import hs.ddif.core.bind.Binding;
 import hs.ddif.core.bind.Key;
@@ -7,7 +8,9 @@ import hs.ddif.core.inject.store.ClassInjectable;
 import hs.ddif.core.store.Injectable;
 import hs.ddif.core.store.InjectableStore;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class DependencySorter {
@@ -15,8 +18,24 @@ public class DependencySorter {
   public static List<Class<?>> getInTopologicalOrder(InjectableStore<Injectable> store, Set<ClassInjectable> classInjectables) {
     DirectedGraph<Class<?>> dg = new DirectedGraph<>();
 
+    /*
+     * Keep track of a Map which links a Producer class to the annotated production result class (the
+     * class with the Producer annotation on it).  This is needed because the Producer class is often
+     * a dependency injected into other classes, and if so, the class carrying the Producer annotation
+     * must be registered first as this registration will automatically register the Producer class
+     * (which is depended on) as well.
+     */
+
+    Map<Class<?>, Class<?>> annotatedClassByProducerClass = new HashMap<>();
+
     for(ClassInjectable injectable : classInjectables) {
       dg.addNode(injectable.getInjectableClass());
+
+      Producer producer = injectable.getInjectableClass().getAnnotation(Producer.class);
+
+      if(producer != null) {
+        annotatedClassByProducerClass.put(producer.value(), injectable.getInjectableClass());
+      }
     }
 
     for(ClassInjectable classInjectable : classInjectables) {
@@ -35,6 +54,18 @@ public class DependencySorter {
               }
 
               dg.addEdge(requiredClass, classInjectable.getInjectableClass());
+            }
+
+            /*
+             * Also create link between the class with Producer annotation and the class depending on the
+             * Producer.  The store.resolve call won't find this relationship as the Producer itself was
+             * not added to the store, only its annotated result class.
+             */
+
+            Class<?> producerAnnotatedClass = annotatedClassByProducerClass.get(requiredKey.getType());
+
+            if(producerAnnotatedClass != null) {
+              dg.addEdge(producerAnnotatedClass, classInjectable.getInjectableClass());
             }
           }
         }
--- ddif-plugins/src/main/java/hs/ddif/plugins/PluginManager.java
+++ ddif-plugins/src/main/java/hs/ddif/plugins/PluginManager.java
@@ -97,6 +97,7 @@ public class PluginManager {
 
       classNames.addAll(reflections.getStore().get("TypeAnnotationsScanner").get("javax.inject.Named"));
       classNames.addAll(reflections.getStore().get("TypeAnnotationsScanner").get("javax.inject.Singleton"));
+      classNames.addAll(reflections.getStore().get("TypeAnnotationsScanner").get("hs.ddif.core.Producer"));
 
       for(String name : reflections.getStore().get("FieldAnnotationsScanner").get("javax.inject.Inject")) {
         classNames.add(name.substring(0, name.lastIndexOf('.')));
