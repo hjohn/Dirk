b0794b8aebaa707fd40e84754da9263ba006412c
John Hendrikx <hjohn@xs4all.nl>
2021-11-06 22:54:29 +0100

    Add underscore quoting support

--- ddif-core/src/test/java/hs/ddif/core/util/ReplaceCamelCaseDisplayNameGenerator.java
+++ ddif-core/src/test/java/hs/ddif/core/util/ReplaceCamelCaseDisplayNameGenerator.java
@@ -5,6 +5,13 @@ import java.util.regex.Pattern;
 
 import org.junit.jupiter.api.DisplayNameGenerator;
 
+/**
+ * A {@link DisplayNameGenerator} which converts camel case test names to normal
+ * text. Test names can use underscores to quote parts of the name that should
+ * be kept exactly as written.<p>
+ *
+ * For example: "When_getName_IsCalled" becomes "When getName is called"
+ */
 public class ReplaceCamelCaseDisplayNameGenerator implements DisplayNameGenerator {
   private static final Pattern UPPERCASE_LETTER = Pattern.compile("([A-Z]|[0-9]+)");
 
@@ -24,7 +31,26 @@ public class ReplaceCamelCaseDisplayNameGenerator implements DisplayNameGenerato
   }
 
   private static String toName(String text) {
-    return text.substring(0, 1).toUpperCase()
-      + UPPERCASE_LETTER.matcher(text.substring(1)).replaceAll(mr -> " " + (mr.group(1).toLowerCase()));
+    StringBuilder sb = new StringBuilder();
+    String[] split = text.split("_");
+
+    for(int i = 0; i < split.length; i++) {
+      String part = split[i];
+
+      if(i > 0) {
+        sb.append(" ");
+      }
+
+      if(i % 2 == 0) {
+        sb.append(UPPERCASE_LETTER.matcher(part).replaceAll(mr -> " " + mr.group(1).toLowerCase()).trim());
+      }
+      else {
+        sb.append(part);
+      }
+    }
+
+    sb.replace(0, 1, sb.substring(0, 1).toUpperCase());
+
+    return sb.toString();
   }
 }
