2286ffe09595660f2a8b991d7f9201d0308d9291
John Hendrikx <hjohn@xs4all.nl>
2022-03-05 15:54:14 +0100

    Add module with support for jakarta annotations

---
+++ ddif-jakarta/src/main/java/hs/ddif/jakarta/Injectors.java
@@ -0,0 +1,138 @@
+package hs.ddif.jakarta;
+
+import hs.ddif.core.Injector;
+import hs.ddif.core.config.AssistedInjectableExtension;
+import hs.ddif.core.config.ConfigurableAnnotationStrategy;
+import hs.ddif.core.config.ListTypeExtension;
+import hs.ddif.core.config.ProducesInjectableExtension;
+import hs.ddif.core.config.ProviderInjectableExtension;
+import hs.ddif.core.config.SetTypeExtension;
+import hs.ddif.core.config.discovery.DiscovererFactory;
+import hs.ddif.core.config.scope.SingletonScopeResolver;
+import hs.ddif.core.config.standard.AnnotationBasedLifeCycleCallbacksFactory;
+import hs.ddif.core.config.standard.DefaultDiscovererFactory;
+import hs.ddif.core.config.standard.DefaultInjectableFactory;
+import hs.ddif.core.config.standard.InjectableExtension;
+import hs.ddif.core.definition.ClassInjectableFactory;
+import hs.ddif.core.definition.FieldInjectableFactory;
+import hs.ddif.core.definition.InjectableFactory;
+import hs.ddif.core.definition.InstanceInjectableFactory;
+import hs.ddif.core.definition.LifeCycleCallbacksFactory;
+import hs.ddif.core.definition.MethodInjectableFactory;
+import hs.ddif.core.definition.bind.AnnotationStrategy;
+import hs.ddif.core.definition.bind.BindingProvider;
+import hs.ddif.core.instantiation.TypeExtension;
+import hs.ddif.core.instantiation.TypeExtensionStore;
+import hs.ddif.core.scope.ScopeResolver;
+import hs.ddif.core.scope.ScopeResolverManager;
+import hs.ddif.core.util.Annotations;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Stream;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import jakarta.inject.Inject;
+import jakarta.inject.Provider;
+import jakarta.inject.Qualifier;
+import jakarta.inject.Scope;
+import jakarta.inject.Singleton;
+
+/**
+ * Factory for {@link Injector}s of various types.
+ */
+public class Injectors {
+  private static final Inject INJECT = Annotations.of(Inject.class);
+  private static final Qualifier QUALIFIER = Annotations.of(Qualifier.class);
+  private static final Scope SCOPE = Annotations.of(Scope.class);
+  private static final Singleton SINGLETON = Annotations.of(Singleton.class);
+  private static final AnnotationStrategy ANNOTATION_STRATEGY = new ConfigurableAnnotationStrategy(INJECT, QUALIFIER, SCOPE);
+  private static final Method PROVIDER_METHOD;
+
+  static {
+    try {
+      PROVIDER_METHOD = Provider.class.getDeclaredMethod("get");
+    }
+    catch(NoSuchMethodException | SecurityException e) {
+      throw new IllegalStateException(e);
+    }
+  }
+
+  /**
+   * Creates an {@link Injector} with auto discovery activated and the given
+   * {@link ScopeResolver}s.
+   *
+   * @param scopeResolvers an optional array of {@link ScopeResolver}s
+   * @return an {@link Injector}, never {@code null}
+   */
+  public static Injector autoDiscovering(ScopeResolver... scopeResolvers) {
+    SingletonScopeResolver singletonScopeResolver = new SingletonScopeResolver(SINGLETON);
+    ScopeResolverManager scopeResolverManager = createScopeResolverManager(singletonScopeResolver, scopeResolvers);
+    InjectableFactory injectableFactory = new DefaultInjectableFactory(scopeResolverManager, ANNOTATION_STRATEGY);
+    InstanceInjectableFactory instanceInjectableFactory = new InstanceInjectableFactory(injectableFactory, SINGLETON);
+
+    return new Injector(
+      createTypeExtensionStore(),
+      createDiscoveryFactory(injectableFactory, true),
+      instanceInjectableFactory
+    );
+  }
+
+  /**
+   * Creates an {@link Injector} which must be manually configured with the given
+   * {@link ScopeResolver}s.
+   *
+   * @param scopeResolvers an optional array of {@link ScopeResolver}s
+   * @return an {@link Injector}, never {@code null}
+   */
+  public static Injector manual(ScopeResolver... scopeResolvers) {
+    SingletonScopeResolver singletonScopeResolver = new SingletonScopeResolver(SINGLETON);
+    ScopeResolverManager scopeResolverManager = createScopeResolverManager(singletonScopeResolver, scopeResolvers);
+    InjectableFactory injectableFactory = new DefaultInjectableFactory(scopeResolverManager, ANNOTATION_STRATEGY);
+    InstanceInjectableFactory instanceInjectableFactory = new InstanceInjectableFactory(injectableFactory, SINGLETON);
+
+    return new Injector(
+      createTypeExtensionStore(),
+      createDiscoveryFactory(injectableFactory, false),
+      instanceInjectableFactory
+    );
+  }
+
+  private static TypeExtensionStore createTypeExtensionStore() {
+    Map<Class<?>, TypeExtension<?>> typeExtensions = new HashMap<>();
+
+    typeExtensions.put(List.class, new ListTypeExtension<>());
+    typeExtensions.put(Set.class, new SetTypeExtension<>());
+    typeExtensions.put(Provider.class, new ProviderTypeExtension<>());
+
+    return new TypeExtensionStore(typeExtensions);
+  }
+
+  private static ScopeResolverManager createScopeResolverManager(SingletonScopeResolver singletonScopeResolver, ScopeResolver... scopeResolvers) {
+    ScopeResolver[] standardScopeResolvers = new ScopeResolver[] {singletonScopeResolver};
+    ScopeResolver[] extendedScopeResolvers = Stream.of(scopeResolvers, standardScopeResolvers).flatMap(Stream::of).toArray(ScopeResolver[]::new);
+
+    return new ScopeResolverManager(extendedScopeResolvers);
+  }
+
+  private static DiscovererFactory createDiscoveryFactory(InjectableFactory injectableFactory, boolean autoDiscovery) {
+    BindingProvider bindingProvider = new BindingProvider(ANNOTATION_STRATEGY);
+    LifeCycleCallbacksFactory lifeCycleCallbacksFactory = new AnnotationBasedLifeCycleCallbacksFactory(ANNOTATION_STRATEGY, PostConstruct.class, PreDestroy.class);
+    ClassInjectableFactory classInjectableFactory = new ClassInjectableFactory(bindingProvider, injectableFactory, lifeCycleCallbacksFactory);
+    MethodInjectableFactory methodInjectableFactory = new MethodInjectableFactory(bindingProvider, injectableFactory);
+    FieldInjectableFactory fieldInjectableFactory = new FieldInjectableFactory(bindingProvider, injectableFactory);
+
+    List<InjectableExtension> injectableExtensions = List.of(
+      new ProviderInjectableExtension(PROVIDER_METHOD, methodInjectableFactory),
+      new ProducesInjectableExtension(methodInjectableFactory, fieldInjectableFactory),
+      new AssistedInjectableExtension(classInjectableFactory, INJECT, Provider.class, Provider::get)
+    );
+
+    return new DefaultDiscovererFactory(autoDiscovery, injectableExtensions, classInjectableFactory);
+  }
+}
+
---
+++ ddif-jakarta/src/test/java/hs/ddif/jakarta/ProviderTypeExtensionInjectorTest.java
@@ -0,0 +1,97 @@
+package hs.ddif.jakarta;
+
+import hs.ddif.core.Injector;
+
+import org.junit.jupiter.api.Test;
+
+import jakarta.inject.Inject;
+import jakarta.inject.Provider;
+import jakarta.inject.Singleton;
+
+public class ProviderTypeExtensionInjectorTest {
+  private Injector injector = Injectors.manual();
+
+  // Singleton -> Singleton = OK
+  @Test
+  public void shouldRegisterSingletonBeanDependentOnXWhenXIsProvidedAsSingleton() {
+    injector.registerInstance(new X());
+    injector.register(SingletonBeanDependentOnX.class);
+  }
+
+  // Prototype -> Prototype = OK
+  @Test
+  public void shouldRegisterPrototypeBeanDependentOnXWhenXIsProvidedAsPrototype() {
+    injector.registerInstance(new XProvider());
+    injector.register(PrototypeBeanDependantOnX.class);
+  }
+
+  // Prototype -> Singleton = OK
+  @Test
+  public void shouldRegisterPrototypeBeanDependentOnXWhenXIsProvidedAsSingleton() {
+    injector.registerInstance(new X());
+    injector.register(PrototypeBeanDependantOnX.class);
+  }
+
+  // Singleton -> Prototype = OK
+  @Test
+  public void shouldThrowExceptionWhenRegisteringSingletonBeanDependentOnXWhenXIsProvidedAsPrototype() {
+    injector.registerInstance(new XProvider());
+    injector.register(SingletonBeanDependentOnX.class);
+  }
+
+  // Singleton -> Provider<Singleton> = OK
+  @Test
+  public void shouldRegisterSingletonBeanDependentOnXProviderWhenXIsProvidedAsSingleton() {
+    injector.registerInstance(new X());
+    injector.register(SingletonBeanDependentOnXProvider.class);
+  }
+
+  // Prototype -> Provider<Prototype> = OK
+  @Test
+  public void shouldRegisterPrototypeBeanDependentOnXProviderWhenXIsProvidedAsPrototype() {
+    injector.registerInstance(new XProvider());
+    injector.register(PrototypeBeanDependantOnXProvider.class);
+  }
+
+  // Prototype -> Provider<Singleton> = OK
+  @Test
+  public void shouldRegisterPrototypeBeanDependentOnXProviderWhenXIsProvidedAsSingleton() {
+    injector.registerInstance(new X());
+    injector.register(PrototypeBeanDependantOnXProvider.class);
+  }
+
+  // Singleton -> Provider<Prototype> = OK
+  @Test
+  public void shouldRegisterSingletonBeanDependentOnXProviderWhenXIsProvidedAsPrototype() {
+    injector.registerInstance(new XProvider());
+    injector.register(SingletonBeanDependentOnXProvider.class);
+  }
+
+  public static class X {
+  }
+
+  public static class PrototypeBeanDependantOnX {
+    @Inject X x;
+  }
+
+  @Singleton
+  public static class SingletonBeanDependentOnX {
+    @Inject X x;
+  }
+
+  public static class PrototypeBeanDependantOnXProvider {
+    @Inject Provider<X> xProvider;
+  }
+
+  @Singleton
+  public static class SingletonBeanDependentOnXProvider {
+    @Inject Provider<X> xProvider;
+  }
+
+  public static class XProvider implements Provider<X> {  // A provider by default is assumed to provide a new bean everytime (the most narrow scope, or prototype scope)
+    @Override
+    public X get() {
+      return null;
+    }
+  }
+}
